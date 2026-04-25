f4fe466c3e987c8f98b4aa75300709d5781d68a1
Armen Daghents <214229177+Daghents-Armen@users.noreply.github.com>
2026-04-26 01:34:51 +0400

    feat: Add dto, app and service

---
+++ notification-service/src/main/java/com/example/chat/notification/dto/ChatMessageEvent.java
@@ -0,0 +1,14 @@
+package com.example.chat.notification.dto;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class ChatMessageEvent {
+    private UUID id;
+    private UUID roomId;
+    private String senderUsername;
+    private String content;
+    private LocalDateTime timestamp;
+}
---
+++ notification-service/src/main/java/com/example/chat/notification/service/NotificationListener.java
@@ -0,0 +1,18 @@
+package com.example.chat.notification.service;
+
+import com.example.chat.notification.dto.ChatMessageEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class NotificationListener {
+    @KafkaListener(topics = "chat-messages-topic", groupId = "notification-group-v2")
+    public void handleNewMessage(ChatMessageEvent event) {
+        log.info("Activity in Room: {}", event.getRoomId());
+        log.info("User [{}] sent a message.", event.getSenderUsername());
+        log.info("Message preview: '{}'", event.getContent());
+        log.info("Action: Simulating push notification to offline members...");
+    }
+}
